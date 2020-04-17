4692a0880438b43dadbec76d2fd6399e2b7233e2
Alex Karavil <alex@karavil.io>
2020-04-17 13:31:19 -0400

    Changed schema to include descriptions for some entries, currently working on express api

--- src/index.ts
+++ src/index.ts
@@ -1,94 +1,91 @@
-import { PrismaClient } from '@prisma/client'
-import * as bodyParser from 'body-parser'
-import express from 'express'
+import { PrismaClient } from "@prisma/client";
+import * as bodyParser from "body-parser";
+import express from "express";
 
-const prisma = new PrismaClient()
-const app = express()
+const prisma = new PrismaClient();
+const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.post(`/user`, async (req, res) => {
-  const result = await prisma.user.create({
-    data: {
-      ...req.body,
-    },
-  })
-  res.json(result)
-})
+app.post(`/projects`, async (req, res) => {
+   const result = await prisma.user.create({
+      data: {
+         ...req.body,
+      },
+   });
+   res.json(result);
+});
 
 app.post(`/post`, async (req, res) => {
-  const { title, content, authorEmail } = req.body
-  const result = await prisma.post.create({
-    data: {
-      title,
-      content,
-      published: false,
-      author: { connect: { email: authorEmail } },
-    },
-  })
-  res.json(result)
-})
+   const { title, content, authorEmail } = req.body;
+   const result = await prisma.post.create({
+      data: {
+         title,
+         content,
+         published: false,
+         author: { connect: { email: authorEmail } },
+      },
+   });
+   res.json(result);
+});
 
-app.put('/publish/:id', async (req, res) => {
-  const { id } = req.params
-  const post = await prisma.post.update({
-    where: { id: Number(id) },
-    data: { published: true },
-  })
-  res.json(post)
-})
+app.put("/publish/:id", async (req, res) => {
+   const { id } = req.params;
+   const post = await prisma.post.update({
+      where: { id: Number(id) },
+      data: { published: true },
+   });
+   res.json(post);
+});
 
 app.delete(`/post/:id`, async (req, res) => {
-  const { id } = req.params
-  const post = await prisma.post.delete({
-    where: {
-      id: Number(id),
-    },
-  })
-  res.json(post)
-})
+   const { id } = req.params;
+   const post = await prisma.post.delete({
+      where: {
+         id: Number(id),
+      },
+   });
+   res.json(post);
+});
 
 app.get(`/post/:id`, async (req, res) => {
-  const { id } = req.params
-  const post = await prisma.post.findOne({
-    where: {
-      id: Number(id),
-    },
-  })
-  res.json(post)
-})
+   const { id } = req.params;
+   const post = await prisma.post.findOne({
+      where: {
+         id: Number(id),
+      },
+   });
+   res.json(post);
+});
 
-app.get('/feed', async (req, res) => {
-  const posts = await prisma.post.findMany({
-    where: { published: true },
-    include: { author: true }
-  })
-  res.json(posts)
-})
+app.get("/projects", async (req, res) => {
+   const projects = await prisma.project.findMany();
+   res.json(projects);
+});
 
-app.get('/filterPosts', async (req, res) => {
-  const { searchString }: { searchString?: string } = req.query;
-  const draftPosts = await prisma.post.findMany({
-    where: {
-      OR: [
-        {
-          title: {
-            contains: searchString,
-          },
-        },
-        {
-          content: {
-            contains: searchString,
-          },
-        },
-      ],
-    },
-  })
-  res.json(draftPosts)
-})
+app.get("/filterPosts", async (req, res) => {
+   const { searchString }: { searchString?: string } = req.query;
+   const draftPosts = await prisma.post.findMany({
+      where: {
+         OR: [
+            {
+               title: {
+                  contains: searchString,
+               },
+            },
+            {
+               content: {
+                  contains: searchString,
+               },
+            },
+         ],
+      },
+   });
+   res.json(draftPosts);
+});
 
-const server = app.listen(3000, () =>
-  console.log(
-    '🚀 Server ready at: http://localhost:3000\n⭐️ See sample requests: http://pris.ly/e/ts/rest-express#3-using-the-rest-api',
-  ),
-)
+const server = app.listen(5000, () =>
+   console.log(
+      "🚀 Server ready at: http://localhost:5000\n⭐️ See sample requests: http://pris.ly/e/ts/rest-express#3-using-the-rest-api"
+   )
+);
